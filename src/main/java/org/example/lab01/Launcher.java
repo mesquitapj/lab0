4865a75b5fb7a5d8084312ad6a951cb59ba7110c
Pedro Mesquita <91216202+mesquitapj@users.noreply.github.com>
2026-02-21 22:06:43 +0000

    Estrutura inicial do projecto
    Estrutura inicial do projecto

--- src/main/java/org/example/lab01/Launcher.java
+++ src/main/java/org/example/lab01/Launcher.java
@@ -2,6 +2,7 @@ package org.example.lab01;
 
 import javafx.application.Application;
 
+//Pequena alteração inicial
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
